ecf80946f4eee8589f34555606f73caeec0c3dea
Ming <mchen@cs.stonybrook.edu>
2013-04-21 17:15:01 -0400

    fix pxy_fsalattr_to_fattr4

--- proxy-test/run-proxy.sh
+++ proxy-test/run-proxy.sh
@@ -1,15 +1,11 @@
 #PATHPROG=/usr/local/bin/ganesha.nfsd 
 PATHPROG=MainNFSD/ganesha.nfsd
 
-LOGFILE=/var/log/ganesha.log
-CONFFILE=/etc/ganesha/ganesha.conf
+LOGFILE=/var/log/proxy.ganesha.log
+CONFFILE=/etc/ganesha/proxy.ganesha.conf
 
 prog=ganesha.nfsd
 PID_FILE=${PID_FILE:=/var/run/${prog}.pid}
 LOCK_FILE=${LOCK_FILE:=/var/lock/subsys/${prog}}
 
-#OPTIONS="-d -L $LOGFILE -f $CONFFILE -N NIV_EVENT -p $PID_FILE "
-OPTIONS="-L $LOGFILE -f $CONFFILE -N NIV_FULL_DEBUG "
-
-# $PATHPROG -f /etc/ganesha/ganesha.conf -N NIV_EVENT -L /var/log/ganesha.log
-$PATHPROG -L /var/log/proxy.ganesha.log -f /etc/ganesha/proxy.ganesha.conf -N NIV_DEBUG
+$PATHPROG -L $LOGFILE -f $CONFFILE -N NIV_DEBUG
